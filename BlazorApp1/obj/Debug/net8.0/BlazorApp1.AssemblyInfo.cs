72b14adac914f3d0150ce7afe01992d3d1010ffc
joshitachu <118347322+joshitachu@users.noreply.github.com>
2024-05-23 20:58:20 +0900

    ik snap er niks van

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd793849e3e0ac8089a5c0e43fc9adc77d2eccf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b684f0275bb7a32ffbbdd774606eb88580456de")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
